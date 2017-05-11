d76e93bfb329a80a93ae8b7715937150ba7db0e9
DmitriySh <dashishmakov@gmail.com>
2017-05-11 18:10:32 +0300

    Added test for visited urls

---
+++ src/test/java/ru/shishmakov/UrlTest.java
@@ -0,0 +1,49 @@
+package ru.shishmakov;
+
+import com.google.common.collect.Lists;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.lang.invoke.MethodHandles;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Objects;
+import java.util.Set;
+
+import static org.apache.commons.lang3.StringUtils.EMPTY;
+import static org.apache.commons.lang3.StringUtils.SPACE;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+/**
+ * @author <a href="mailto:d.shishmakov@corp.nekki.ru">Shishmakov Dmitriy</a>
+ */
+public class UrlTest extends BaseTest {
+    private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    @Test
+    public void visitedUrlsShouldDefineOnlyUniquePath() throws Exception {
+        Set<String> visited = new HashSet<>();
+        visited.add("https://yandex.ru");
+        List<String> sources = Lists.newArrayList(
+                "https://yandex.ru",
+                "https://yandex.ru:443/maps/213/moscow/",
+                "HTTPS://YANDEX.RU/MAPS/213/MOSCOW/", // equal
+                "https://yandex.ru/maps/213/moscow", // equal
+                "https://yandex.ru/maps/213/moscow/?rtext=&rtt=auto&mode=routes", // equal
+                "https://yandex.ru/maps/43/kazan/?mode=search&text=kazan",
+                EMPTY, SPACE, null);
+        sources.stream()
+                .map(StringUtils::trimToNull)
+                .filter(Objects::nonNull)
+                .map(s -> StringUtils.substringBefore(s, "?"))
+                .map(s -> StringUtils.removeEnd(s, "/"))
+                .map(StringUtils::lowerCase)
+                .forEach(visited::add);
+
+        assertEquals("Invalid number of unique URLs", 4, visited.size());
+        visited.forEach(t -> assertTrue("URL should not be blank or null", StringUtils.isNotBlank(t)));
+    }
+}
